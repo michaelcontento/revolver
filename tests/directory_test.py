baefcc9f991ede7c4dc31d62a1fb9d4ff697fd9e
Michael Contento <michaelcontento@gmail.com>
2012-01-24 21:36:16 +0100

    Tests for revolver.directory added

---
+++ tests/directory_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from cuisine import dir_attribs as cuisine_attributes
+from cuisine import dir_ensure as cuisine_ensure
+from cuisine import dir_exists as cuisine_exists
+from cuisine import file_attribs_get as cuisine_attributes_get
+
+from revolver import directory
+
+def test_revolver_just_wrapps_cuisine():
+    assert directory.attributes == cuisine_attributes
+    assert directory.attributes_get == cuisine_attributes_get
+    assert directory.ensure == cuisine_ensure
+    assert directory.exists == cuisine_exists
