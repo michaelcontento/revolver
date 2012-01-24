161d5d9adb7ffe66799372e6af4ababe397ed62e
Michael Contento <michaelcontento@gmail.com>
2012-01-24 22:05:00 +0100

    Tests for revolver.text added

---
+++ tests/text_test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from cuisine import text_ensure_line as cuisine_ensure_line
+from fabric.utils import indent as fabric_indent
+
+from revolver import text
+
+def test_revolver_just_wrapps_cuisine():
+    assert text.ensure_line == cuisine_ensure_line
+    assert text.indent == fabric_indent
