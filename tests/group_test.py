f7a59ec8c6b815d6afec31491c8d261b22d83a2f
Michael Contento <michaelcontento@gmail.com>
2012-01-24 21:42:39 +0100

    Tests for revolver.group added

---
+++ tests/group_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from cuisine import group_check as cuisine_get
+from cuisine import group_create as cuisine_create
+from cuisine import group_ensure as cuisine_ensure
+from cuisine import group_user_add as cuisine_user_add
+from cuisine import group_user_check as cuisine_user_check
+from cuisine import group_user_ensure as cuisine_user_ensure
+
+from revolver import group
+
+def test_revolver_just_wrapps_cuisine():
+    assert group.get == cuisine_get
+    assert group.create == cuisine_create
+    assert group.ensure == cuisine_ensure
+    assert group.user_add == cuisine_user_add
+    assert group.user_check == cuisine_user_check
+    assert group.user_ensure == cuisine_user_ensure
