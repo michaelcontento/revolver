6ff20895158af35dd5776a9be67d696eddf87e16
Michael Contento <michaelcontento@gmail.com>
2012-01-26 22:59:53 +0100

    Variable naming simplified in contextmanager tests

--- revolver/test/contextmanager_test.py
+++ revolver/test/contextmanager_test.py
@@ -1,33 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from fabric import context_managers as old_ctx
+from fabric import context_managers as fabric_ctx
 
-from revolver import contextmanager as new_ctx
+from revolver import contextmanager as ctx
 from revolver.core import env
 
 def test_revolver_is_just_a_wrapper():
-    assert new_ctx.cd == old_ctx.cd
-    assert new_ctx.hide == old_ctx.hide
-    assert new_ctx.lcd == old_ctx.lcd
-    assert new_ctx.path == old_ctx.path
-    assert new_ctx.prefix == old_ctx.prefix
-    assert new_ctx.settings == old_ctx.settings
-    assert new_ctx.show == old_ctx.show
+    assert ctx.cd == fabric_ctx.cd
+    assert ctx.hide == fabric_ctx.hide
+    assert ctx.lcd == fabric_ctx.lcd
+    assert ctx.path == fabric_ctx.path
+    assert ctx.prefix == fabric_ctx.prefix
+    assert ctx.settings == fabric_ctx.settings
+    assert ctx.show == fabric_ctx.show
 
 def test_sudo_changes_env_flag():
-    with new_ctx.sudo():
+    with ctx.sudo():
         assert env.sudo_forced
     
 def test_sudo_without_user_does_not_change_sudo_env_user():
-    with new_ctx.sudo():
+    with ctx.sudo():
         assert env.sudo_user == None
 
 def test_sudo_with_user_change_sudo_env_user():
-    with new_ctx.sudo("foo"):
+    with ctx.sudo("foo"):
         assert env.sudo_user == "foo"
 
 def test_sudo_restores_previous_settings():
-    with new_ctx.sudo("foo"):
+    with ctx.sudo("foo"):
         pass
 
     assert not env.sudo_forced
