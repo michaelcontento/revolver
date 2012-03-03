daf8196674471314599f0b96d0e3c6c0616983fa
Michael Contento <michaelcontento@gmail.com>
2012-03-03 01:32:58 +0100

    New contextmanager fake_login and rbenv added

--- revolver/contextmanager.py
+++ revolver/contextmanager.py
@@ -10,6 +10,7 @@ from fabric.context_managers import prefix
 from fabric.context_managers import settings
 from fabric.context_managers import show
 
+from revolver import user
 from revolver.core import env
 
 @contextmanager
@@ -24,3 +25,17 @@ def sudo(user=None):
 
     env.sudo_forced = old_forced
     env.sudo_user = old_user
+
+@contextmanager
+def fake_login(username):
+    home = user.home_directory(username)
+    fake_home = "export HOME=%s" % home
+    with sudo(username), cd(home), prefix(fake_home):
+        yield
+
+@contextmanager
+def rbenv():
+    path = 'export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"'
+    with prefix(path):
+        yield
+
