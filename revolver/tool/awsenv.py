d4b97ec7a504d64a644fda1c29ef33bcd060bde8
Michael Contento <michaelcontento@gmail.com>
2012-03-05 23:17:35 +0100

    tool/awsenv added

---
+++ revolver/tool/awsenv.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*- 
+
+from __future__ import with_statement
+
+from revolver import contextmanager as ctx
+from revolver import directory as dir
+from revolver import package
+from revolver.core import run
+
+def install():
+    package.ensure(["git-core", "openjdk-7-jre"])
+
+    if not dir.exists(".awsenv"):
+        run("git clone git://github.com/michaelcontento/awsenv.git .awsenv")
+        return
+
+    with ctx.cd(".awsenv"):
+        run("git pull")
+
+def ensure():
+    if not dir.exists(".awsenv"):
+        install()
