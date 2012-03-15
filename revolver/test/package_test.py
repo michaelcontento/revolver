28fa440cf52fca605ebb22fa96692fe65ffb745f
Michael Contento <michaelcontento@gmail.com>
2012-03-15 22:21:44 +0000

    Tests fixed

--- revolver/test/package_test.py
+++ revolver/test/package_test.py
@@ -6,7 +6,9 @@ from cuisine import package_update as cuisine_update
 
 from revolver import package
 
+from .utils import assert_contain_function_wrapped
+
 def test_revolver_is_just_a_wrapper():
-    assert package.ensure == cuisine_ensure
-    assert package.install == cuisine_install
-    assert package.update == cuisine_update
+    assert_contain_function_wrapped(package.ensure, cuisine_ensure)
+    assert_contain_function_wrapped(package.install, cuisine_install)
+    assert_contain_function_wrapped(package.update, cuisine_update)
