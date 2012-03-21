d7edfbc8ff2d5cfa82cc9d9244526154ba011f1d
Michael Contento <michaelcontento@gmail.com>
2012-03-21 21:35:59 +0000

    Missing tests for revolver.decorator added

--- revolver/test/decorator_test.py
+++ revolver/test/decorator_test.py
@@ -3,6 +3,9 @@
 from fabric import decorators as old_decorator
 from fabric.network import needs_host as fabric_needs_host
 
+from revolver.decorator import inject_use_sudo
+from revolver.decorator import multiargs
+from revolver import contextmanager as ctx
 from revolver import decorator
 
 def test_revolver_is_just_a_wrapper():
@@ -14,3 +17,43 @@ def test_revolver_is_just_a_wrapper():
     assert decorator.serial == old_decorator.serial
     assert decorator.task == old_decorator.task
     assert decorator.with_settings == old_decorator.with_settings
+
+def test_multiargs():
+    args = []
+    def dummy(arg):
+        args.append(arg)
+
+    multiargs(dummy)([1, 2, 3])
+    assert args == [1, 2, 3]
+
+def _sudo_dummy(use_sudo=None):
+    return use_sudo
+
+def test_sudo_dummy():
+    assert _sudo_dummy() == None
+    assert _sudo_dummy(True)
+    assert not _sudo_dummy(False)
+
+def test_inject_sudo_with_forced_sudo():
+    with ctx.sudo():
+        assert inject_use_sudo(_sudo_dummy)()
+
+def test_inject_sudo_does_nothing_if_argument_given():
+    assert inject_use_sudo(_sudo_dummy)(use_sudo=True)
+    assert not inject_use_sudo(_sudo_dummy)(use_sudo=False)
+
+def _use_sudo_dummy(use_sudo=None):
+    return use_sudo
+
+def test_sudo_dummy():
+    assert _use_sudo_dummy() == None
+    assert _use_sudo_dummy(True)
+    assert not _use_sudo_dummy(False)
+
+def test_inject_use_sudo_with_forced_sudo():
+    with ctx.sudo():
+        assert inject_use_sudo(_use_sudo_dummy)()
+
+def test_inject_use_sudo_does_nothing_if_argument_given():
+    assert inject_use_sudo(_use_sudo_dummy)(use_sudo=True)
+    assert not inject_use_sudo(_use_sudo_dummy)(use_sudo=False)
