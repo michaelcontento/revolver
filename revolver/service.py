cb6b907e178621a4a31e98f012150ccec45b5a2b
Michael Contento <michaelcontento@gmail.com>
2012-03-24 21:25:35 +0000

    Tests for revolver.service added

--- revolver/service.py
+++ revolver/service.py
@@ -3,27 +3,25 @@
 from __future__ import absolute_import, division, with_statement
 
 from revolver import contextmanager as ctx
-from revolver.core import sudo
+from revolver import core
 
 def command(name, command):
-    sudo(
-        '/etc/init.d/%(name)s %(command)s'
-        % {'name': name, 'command': command}
-    )
+    cmd = "/etc/init.d/%s %s" % (name, command)
+    core.sudo(cmd)
 
 def start(name):
-    command(name, 'start')
+    command(name, "start")
 
 def stop(name):
-    command(name, 'stop')
+    command(name, "stop")
 
 def restart(name):
-    command(name, 'restart')
+    command(name, "restart")
 
 def reload(name):
-    command(name, 'reload')
+    command(name, "reload")
 
 def is_running(name):
     with ctx.settings(warn_only=True):
-        res = sudo('/etc/init.d/%s status' % name)
+        res = core.sudo("/etc/init.d/%s status" % name)
         return res.succeeded
---
+++ revolver/test/service_test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, with_statement
+
+from fudge import patch
+import fabric
+
+from revolver import service
+
+@patch("revolver.core.sudo")
+def test_is_running(sudo):
+    command = "/etc/init.d/foo status"
+    result = fabric.operations._AttributeList("")
+    result.succeeded = "succeeded"
+
+    sudo.expects_call().with_args(command).returns(result)
+    assert service.is_running("foo") == "succeeded"
+
+@patch("revolver.core.sudo")
+def test_command(sudo):
+    sudo.expects_call().with_args("/etc/init.d/foo method")
+    service.command("foo", "method")
+
+@patch("revolver.service.command")
+def test_start(command):
+    command.expects_call().with_args("foo", "start")
+    service.start("foo")
+
+@patch("revolver.service.command")
+def test_stop(command):
+    command.expects_call().with_args("foo", "stop")
+    service.stop("foo")
+
+@patch("revolver.service.command")
+def test_restart(command):
+    command.expects_call().with_args("foo", "restart")
+    service.restart("foo")
+
+@patch("revolver.service.command")
+def test_reload(command):
+    command.expects_call().with_args("foo", "reload")
+    service.reload("foo")
+
